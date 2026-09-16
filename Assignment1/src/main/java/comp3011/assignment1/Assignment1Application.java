5169293bfe0fc15c61a1016cf590c204bf6e2963
luona001 <luona001@mymail.unisa.edu.au>
2026-09-16 12:03:09 +0930

    Write comments for file Assignment1Application.java

--- Assignment1/src/main/java/comp3011/assignment1/Assignment1Application.java
+++ Assignment1/src/main/java/comp3011/assignment1/Assignment1Application.java
@@ -3,6 +3,13 @@ package comp3011.assignment1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * COMP3011 Assignment 1 - Speech-to-Text Web Application
+ * 
+ * Entry point for the Spring Boot application.
+ * Configures virtual threads for high concurrency and initializes
+ * all REST API endpoints for transcription, statistics, and administration.
+ */
 @SpringBootApplication
 public class Assignment1Application {
 
